e660f5afe610fb30875945cae5e128316b826f21
Milad-Soleymani <miladcoder1390@gmail.com>
2026-07-21 20:25:16 +0330

    Add & Use logo in site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Azar Bonian Marava Co",
   description: "",
+   icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
