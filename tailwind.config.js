d0bea31f567c00a5ff55f6a052205edd937d663e
Milad-Soleymani <miladcoder1390@gmail.com>
2026-07-22 12:07:05 +0330

    Config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "15px"
+    },
+    sreens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
