ca29ef5a45bdd7f82cea0950ecce9a249030f694
Milad-Soleymani <miladcoder1390@gmail.com>
2026-07-22 14:07:16 +0330

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,34 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+// components
+import Nav from "../components/Nav";
+
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-      <div className="container mx-auto">
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
-          <h1 className="text-4xl font-semibold">
-            Azar Bonian Marava
-            <span className="text-accent">
-                Co.
-            </span>
-             </h1>
+          <h1 className="text-2xl font-semibold">Azar Bonian Marava</h1>
+          <span className="text-accent text-left">Co.</span>
         </Link>
+        {/* Desktop Navbar & button*/}
+
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Contact Us</Button>
+          </Link>
+        </div>
+
+
+        {/* Mobile Navbar  */}
+
+<div className="xl:hidden">
+    mobile nav
+</div>
+
       </div>
     </header>
   );
