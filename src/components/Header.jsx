17ddf1150af6c286a81be02ad59e02c586b824ea
Milad-Soleymani <miladcoder1390@gmail.com>
2026-07-22 13:22:19 +0330

    Make header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,22 @@
- const Header = () => {
-    return <header> Header </header>
-  
-}
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+      <div className="container mx-auto">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Azar Bonian Marava
+            <span className="text-accent">
+                Co.
+            </span>
+             </h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
