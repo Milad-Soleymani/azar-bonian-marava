180da611488140af4ce0a9efe40f2fbafeb3f582
Milad-Soleymani <miladcoder1390@gmail.com>
2026-07-23 20:09:04 +0330

    Use blog components

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,28 @@
+"use client";
 
-const Blog = () => {
-  return <div>
-      Blog page
-    </div>
-  
-}
+import { useState } from "react";
 
-export default Blog
+import BlogGrid from "@/components/blog/BlogGrid";
+import BlogHeader from "@/components/blog/BlogHeader";
+import Pagination from "@/components/blog/Pagination";
 
+const BlogPage = () => {
+  const [search, setSearch] = useState("");
 
+  return (
+    <main className="min-h-screen bg-[#0A0A0A]">
+      <BlogHeader
+        search={search}
+        setSearch={setSearch}
+      />
+
+      <BlogGrid
+        search={search}
+      />
+
+      <Pagination />
+    </main>
+  );
+};
+
+export default BlogPage;
