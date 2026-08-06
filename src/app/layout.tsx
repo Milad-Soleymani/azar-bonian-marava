e62452d412c63ff21cfb4b12a6fc17ac48a1d4ee
Milad Coder <miladcoder1390@gmail.com>
2026-08-06 18:33:46 +0330

    Debug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -125,50 +125,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
-  return (
-   <html>
-    <meta name="google-site-verification" content="qUW9d1djKLfxa5JvCDMEpkcyzcKjQo09k6x2Usupxfs"/>
-<meta name="y_key" content="yahoo" />
-<meta name="yandex-verification" content="yandex" />
-<meta name="me" content="miladcoder0@gmail.com" />
-<meta name="me" content="azar-bonian-marava.vercel.app" />
-      <head>
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-        >
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K7N7T2WP');
-          `}
-        </Script>
-        <Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-K2LX2N4GK6"
-  strategy="afterInteractive"
-/>
+return (
+  <html>
+    <head>
+      <meta
+        name="google-site-verification"
+        content="qUW9d1djKLfxa5JvCDMEpkcyzcKjQo09k6x2Usupxfs"
+      />
 
-<Script id="google-analytics" strategy="afterInteractive">
-{`
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-K2LX2N4GK6"
+        strategy="afterInteractive"
+      />
 
-gtag('config', 'G-K2LX2N4GK6');
-`}
-</Script>
-      </head>
-      <body
-      className={`${inter.variable} ${vazirmatn.variable}`}>
-        {children}
-      </body>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-    </html>
-  );
+          gtag('config', 'G-K2LX2N4GK6');
+        `}
+      </Script>
+
+    </head>
+
+    <body className={`${inter.variable} ${vazirmatn.variable}`}>
+      {children}
+    </body>
+  </html>
+);
 }
